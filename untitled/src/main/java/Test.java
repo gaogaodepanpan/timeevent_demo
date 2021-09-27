f10d120263b6c0ae754bd2a8cd84af8abc38ab6d
gaogaodepanpan <gp919212418@163.com>
2021-09-27 14:18:20 +0800

    Update Test.java

--- untitled/src/main/java/Test.java
+++ untitled/src/main/java/Test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("pull learn a ");
+        System.out.println("pull learn a b ");
 
     }
 }
